ab58225540042716d6e650a76eb88ba9bd7b0e55
Mikael <git@mikaelmello.com>
2021-06-15 00:35:53 -0300

    Add config and its default

---
+++ src/config.rs
@@ -0,0 +1,23 @@
+pub struct PromptConfig {
+    pub page_size: usize,
+    pub help_input: String,
+    pub filter: fn(filter: &str, value: &str, index: usize) -> bool,
+    pub keep_filter: bool,
+    pub show_cursor: bool,
+}
+
+impl Default for PromptConfig {
+    fn default() -> Self {
+        Self {
+            page_size: 7,
+            help_input: String::from("?"),
+            filter: |filter: &str, value: &str, _| -> bool {
+                let filter = filter.to_lowercase();
+
+                value.to_lowercase().contains(&filter)
+            },
+            keep_filter: false,
+            show_cursor: false,
+        }
+    }
+}
