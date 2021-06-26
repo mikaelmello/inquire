ff159d5cc2827ca389ff53a8dcfac640544acb53
Mikael Mello <git@mikaelmello.com>
2021-06-26 03:26:58 -0300

    Delete stale code

--- src/config.rs
+++ src/config.rs
@@ -1,23 +1,5 @@
-use std::error::Error;
-
-use crate::answer::Answer;
-
 pub type Filter = fn(filter: &str, value: &str, index: usize) -> bool;
-pub type Transformer = fn(answer: &Answer) -> String;
-pub type Validator = fn(answer: &Answer) -> Result<(), Box<dyn Error>>;
 pub type Suggestor = fn(value: &str) -> Vec<String>;
-
-#[derive(Copy, Clone, Default)]
-pub struct PromptConfig<'a> {
-    pub page_size: Option<usize>,
-    pub filter: Option<Filter>,
-    pub confirm_default: Option<bool>,
-    pub transformer: Option<Transformer>,
-    pub validator: Option<Validator>,
-    pub help_message: Option<&'a str>,
-    pub keep_filter: Option<bool>,
-    pub vim_mode: Option<bool>,
-}
 
 pub(in crate) const DEFAULT_PAGE_SIZE: usize = 7;
 pub(in crate) const DEFAULT_VIM_MODE: bool = false;
--- src/prompts/mod.rs
+++ src/prompts/mod.rs
@@ -1,19 +1,11 @@
-pub(in crate) mod confirm;
-pub(in crate) mod multiselect;
-pub(in crate) mod password;
-pub(in crate) mod select;
+pub mod confirm;
+pub mod multiselect;
+pub mod password;
+pub mod select;
 pub mod text;
-
-use std::error::Error;
 
 pub use confirm::Confirm;
 pub use multiselect::MultiSelect;
 pub use password::Password;
 pub use select::Select;
 pub use text::Text;
-
-use crate::{renderer::Renderer, Answer};
-
-pub(in crate) trait Prompt {
-    fn prompt(self, renderer: &mut Renderer) -> Result<Answer, Box<dyn Error>>;
-}
