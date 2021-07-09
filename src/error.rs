d2edf0b22664592b58710fa7b019937c40a573fa
Mikael Mello <git@mikaelmello.com>
2021-07-09 13:59:38 -0300

    feat: add InquireError for custom errors and InquireResult

---
+++ src/error.rs
@@ -0,0 +1,25 @@
+//! Module containing definitions of inquire's error handling
+
+use std::io;
+
+use thiserror::Error;
+
+/// Inquire errors
+#[derive(Error, Debug)]
+pub enum InquireError {
+    /// The input device is not a TTY.
+    #[error("The input device is not a TTY")]
+    NotTTY,
+
+    /// Error when executing IO operations.
+    #[error("IO error")]
+    IO(#[from] io::Error),
+
+    /// Returned when the user canceled the operation, probably
+    /// by pressing Ctrl+C or ESC.
+    #[error("Operation was canceled by the user")]
+    Cancel,
+}
+
+/// Result type where errors are from type InquireError
+pub type InquireResult<T> = Result<T, InquireError>;
