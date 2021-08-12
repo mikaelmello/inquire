2d52c610dd23e040a5441ac904884d79be28cf14
Mikael Mello <git@mikaelmello.com>
2021-08-12 08:58:05 -0300

    refactor: remove old terminal

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -4,13 +4,11 @@ mod backend;
 mod color;
 mod color_theme;
 mod key;
-mod old_terminal;
 mod renderer;
 mod style;
 
 pub(in crate) use backend::*;
 pub(in crate) use key::{Key, KeyModifiers};
-pub(in crate) use old_terminal::OldTerminal;
 pub(in crate) use renderer::Renderer;
 pub(in crate) use style::Styled;
 
