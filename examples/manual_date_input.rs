5831f509ad6adbd0affc5f4ab209fa3c14e299d5
Mikael Mello <git@mikaelmello.com>
2021-08-17 00:32:56 -0300

    example: add manual_date_input example

---
+++ examples/manual_date_input.rs
@@ -0,0 +1,17 @@
+use chrono::NaiveDate;
+use inquire::{formatter::DEFAULT_DATE_FORMATTER, CustomType};
+
+fn main() {
+    let amount = CustomType::<NaiveDate>::new("When are you going to visit the office?")
+        .with_placeholder("dd/mm/yyyy")
+        .with_parser(&|i| NaiveDate::parse_from_str(i, "%d/%m/%Y").map_err(|_| ()))
+        .with_formatter(DEFAULT_DATE_FORMATTER)
+        .with_error_message("Please type a valid date.")
+        .with_help_message("The necessary arrangements will be made")
+        .prompt();
+
+    match amount {
+        Ok(_) => println!("Thanks! We will be expecting you."),
+        Err(_) => println!("We could not process your reservation"),
+    }
+}
