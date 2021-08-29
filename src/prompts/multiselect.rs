dc13d21e62986108ca47415ac3d349a626dc7c80
Mikael Mello <git@mikaelmello.com>
2021-08-29 17:11:56 -0300

    fix: do not call % on length of list of filtered options when it is empty
    Fixes #30

--- src/prompts/multiselect.rs
+++ src/prompts/multiselect.rs
@@ -384,7 +384,9 @@ where
         self.cursor_index = self.cursor_index.saturating_add(qty);
 
         if self.cursor_index >= self.filtered_options.len() {
-            self.cursor_index = if wrap {
+            self.cursor_index = if self.filtered_options.is_empty() {
+                0
+            } else if wrap {
                 self.cursor_index % self.filtered_options.len()
             } else {
                 self.filtered_options.len().saturating_sub(1)
@@ -546,5 +548,74 @@ where
         backend.finish_prompt(&self.message, &formatted)?;
 
         Ok(final_answer)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        formatter::MultiOptionFormatter,
+        list_option::ListOption,
+        ui::{crossterm::CrosstermTerminal, Backend, RenderConfig},
+        MultiSelect,
+    };
+    use crossterm::event::{KeyCode, KeyEvent};
+
+    #[test]
+    /// Tests that a closure that actually closes on a variable can be used
+    /// as a Select formatter.
+    fn closure_formatter() {
+        let read: Vec<KeyEvent> = vec![KeyCode::Char(' '), KeyCode::Enter]
+            .into_iter()
+            .map(KeyEvent::from)
+            .collect();
+        let mut read = read.iter();
+
+        let formatted = String::from("Thanks!");
+        let formatter: MultiOptionFormatter<i32> = &|_| formatted.clone();
+
+        let options = vec![1, 2, 3];
+
+        let mut write: Vec<u8> = Vec::new();
+        let terminal = CrosstermTerminal::new_with_io(&mut write, &mut read);
+        let mut backend = Backend::new(terminal, RenderConfig::default_static_ref()).unwrap();
+
+        let ans = MultiSelect::new("Question", options)
+            .with_formatter(formatter)
+            .prompt_with_backend(&mut backend)
+            .unwrap();
+
+        assert_eq!(vec![ListOption::new(0, 1)], ans);
+    }
+
+    #[test]
+    // Anti-regression test: https://github.com/mikaelmello/inquire/issues/30
+    fn down_arrow_on_empty_list_does_not_panic() {
+        let read: Vec<KeyEvent> = [
+            KeyCode::Char('9'),
+            KeyCode::Down,
+            KeyCode::Backspace,
+            KeyCode::Char('3'),
+            KeyCode::Down,
+            KeyCode::Backspace,
+            KeyCode::Enter,
+        ]
+        .iter()
+        .map(|c| KeyEvent::from(*c))
+        .collect();
+
+        let mut read = read.iter();
+
+        let options = vec![1, 2, 3];
+
+        let mut write: Vec<u8> = Vec::new();
+        let terminal = CrosstermTerminal::new_with_io(&mut write, &mut read);
+        let mut backend = Backend::new(terminal, RenderConfig::default_static_ref()).unwrap();
+
+        let ans = MultiSelect::new("Question", options)
+            .prompt_with_backend(&mut backend)
+            .unwrap();
+
+        assert_eq!(Vec::<ListOption<i32>>::new(), ans);
     }
 }
