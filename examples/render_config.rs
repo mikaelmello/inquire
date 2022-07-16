ab8ebd2569d66fe6cf39aa99dc3025b482573bae
Dominik Nakamura <dnaka91@gmail.com>
2022-07-16 18:10:30 +0900

    Bring validator macros back

--- examples/render_config.rs
+++ examples/render_config.rs
@@ -2,8 +2,8 @@ use std::fmt::{Display, Formatter};
 
 use inquire::{
     error::{CustomUserError, InquireResult},
+    required,
     ui::{Attributes, Color, RenderConfig, StyleSheet, Styled},
-    validator::ValueRequiredValidator,
     CustomType, DateSelect, MultiSelect, Select, Text,
 };
 
@@ -15,7 +15,7 @@ fn main() -> InquireResult<()> {
     let _category = Select::new("Category:", get_categories()).prompt()?;
 
     let _payee = Text::new("Payee:")
-        .with_validator(ValueRequiredValidator::new("This field is required"))
+        .with_validator(required!("This field is required"))
         .with_suggester(&payee_suggestor)
         .with_help_message("e.g. Music Store")
         .with_page_size(5)
