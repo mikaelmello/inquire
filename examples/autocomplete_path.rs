24a6ebce2f546cad520ad2b8ffd737fc7f787a00
Mikael Mello <git@mikaelmello.com>
2022-08-17 07:31:05 -0700

    New completer poc

--- examples/autocomplete_path.rs
+++ examples/autocomplete_path.rs
@@ -1,9 +1,13 @@
-use inquire::{CustomUserError, Text};
+use std::fs::DirEntry;
+
+use inquire::{
+    autocompletion::{AutoComplete, Completion},
+    CustomUserError, Text,
+};
 
 fn main() {
     let ans = Text::new("Profile picture:")
-        .with_suggester(&suggest_file_paths)
-        .with_completer(&complete_file_path)
+        .with_auto_completion(FilePathCompleter::default())
         .prompt();
 
     match ans {
@@ -12,73 +16,112 @@ fn main() {
     }
 }
 
-fn suggest_file_paths(input: &str) -> Result<Vec<String>, CustomUserError> {
-    Ok(list_paths(input)?)
+#[derive(Clone, Default)]
+pub struct FilePathCompleter {
+    paths: Vec<String>,
 }
 
-fn complete_file_path(input: &str) -> Result<Option<String>, CustomUserError> {
-    // Implementation from https://rosettacode.org/wiki/Longest_common_prefix#Rust
-    fn longest_common_prefix<T: AsRef<[u8]>>(list: &[T]) -> Option<Vec<u8>> {
-        if list.is_empty() {
-            return None;
-        }
-        let mut ret = Vec::new();
-        let mut i = 0;
-        loop {
-            let mut c = None;
-            for word in list {
-                let word = word.as_ref();
-                if i == word.len() {
-                    return Some(ret);
-                }
-                match c {
-                    None => {
-                        c = Some(word[i]);
-                    }
-                    Some(letter) if letter != word[i] => return Some(ret),
-                    _ => continue,
-                }
+impl AutoComplete for FilePathCompleter {
+    fn update_input(&mut self, input: &str) -> Result<(), CustomUserError> {
+        let root = input;
+        self.paths.clear();
+
+        let mut input_path = std::path::PathBuf::from(root);
+        if let Some(parent) = input_path.parent() {
+            if !input_path.exists() || !input_path.is_dir() || !root.ends_with('/') {
+                input_path = parent.to_path_buf();
             }
-            if let Some(letter) = c {
-                ret.push(letter);
-            }
-            i += 1;
         }
+        if root.is_empty() {
+            input_path = std::env::current_dir()?;
+        }
+        if !input_path.exists() {
+            return Ok(());
+        }
+
+        let mut entries: Vec<DirEntry> =
+            std::fs::read_dir(input_path)?.collect::<Result<Vec<_>, _>>()?;
+
+        let mut idx = 0;
+        let limit = 15;
+
+        while idx < entries.len() && self.paths.len() < limit {
+            let entry = entries.get(idx).unwrap();
+
+            let path = entry.path();
+            let path_str = path.to_string_lossy();
+
+            if path_str.starts_with(root) {
+                let path = if path.is_dir() {
+                    let mut subentries: Vec<DirEntry> =
+                        std::fs::read_dir(path.clone())?.collect::<Result<Vec<_>, _>>()?;
+                    entries.append(&mut subentries);
+
+                    format!("{}/", path_str)
+                } else {
+                    path_str.to_string()
+                };
+                self.paths.push(path);
+            }
+
+            idx = idx.saturating_add(1);
+        }
+
+        self.paths.sort();
+
+        Ok(())
     }
 
-    Ok(longest_common_prefix(&list_paths(input)?)
-        .map(|bytes| String::from_utf8_lossy(&bytes).to_string()))
+    fn get_suggestions(&self) -> Result<Vec<String>, CustomUserError> {
+        Ok(self.paths.clone())
+    }
+
+    fn get_completion(
+        &self,
+        selected_suggestion: Option<(usize, &str)>,
+    ) -> Result<inquire::autocompletion::Completion, CustomUserError> {
+        let completion = match selected_suggestion {
+            None => {
+                let lcp = longest_common_prefix(&self.paths)
+                    .map(|bytes| String::from_utf8_lossy(&bytes).to_string());
+
+                match lcp {
+                    Some(lcp) => Completion::Replace(lcp),
+                    None => Completion::None,
+                }
+            }
+            Some(suggestion) => Completion::Replace(suggestion.1.to_owned()),
+        };
+
+        Ok(completion)
+    }
 }
 
-fn list_paths(root: &str) -> std::io::Result<Vec<String>> {
-    let mut suggestions = vec![];
-
-    let mut input_path = std::path::PathBuf::from(root);
-    if let Some(parent) = input_path.parent() {
-        if !input_path.exists() || !input_path.is_dir() || !root.ends_with('/') {
-            input_path = parent.to_path_buf();
+// Implementation from https://rosettacode.org/wiki/Longest_common_prefix#Rust
+fn longest_common_prefix<T: AsRef<[u8]>>(list: &[T]) -> Option<Vec<u8>> {
+    if list.is_empty() {
+        return None;
+    }
+    let mut ret = Vec::new();
+    let mut i = 0;
+    loop {
+        let mut c = None;
+        for word in list {
+            let word = word.as_ref();
+            if i == word.len() {
+                return Some(ret);
+            }
+            match c {
+                None => {
+                    c = Some(word[i]);
+                }
+                Some(letter) if letter != word[i] => return Some(ret),
+                _ => continue,
+            }
         }
-    }
-    if root.is_empty() {
-        input_path = std::env::current_dir()?;
-    }
-    if !input_path.exists() {
-        return Ok(vec![]);
-    }
-
-    for entry in std::fs::read_dir(input_path)? {
-        let path = entry?.path();
-        let path_str = path.to_string_lossy();
-
-        if path_str.starts_with(root) {
-            let path = if path.is_dir() {
-                format!("{}/", path_str)
-            } else {
-                path_str.to_string()
-            };
-            suggestions.push(path);
+        if let Some(letter) = c {
+            ret.push(letter);
         }
+        i += 1;
     }
-
-    Ok(suggestions)
 }
