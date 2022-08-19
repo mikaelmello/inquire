07302d7c4a070865122e9dd72a5a2061dfa50f45
Mikael Mello <git@mikaelmello.com>
2022-08-18 19:11:41 -0700

    Add date prompt with custom type parsing

--- examples/date.rs
+++ examples/date.rs
@@ -1,22 +1,63 @@
-use chrono::NaiveDate;
-use inquire::{validator::Validation, DateSelect};
+use chrono::{NaiveDate, Weekday};
+use inquire::{formatter::DEFAULT_DATE_FORMATTER, validator::Validation, CustomType, DateSelect};
 
 fn main() {
-    let date = DateSelect::new("Simple input").prompt().unwrap();
-    println!("{}", date);
+    date_select_default();
+    custom_type_parsed_date_prompt();
+    date_select_misc_options();
+    date_select_with_validation();
+}
 
-    let date = DateSelect::new("Date range input")
-        .with_min_date(NaiveDate::from_ymd(2021, 7, 7))
-        .with_max_date(NaiveDate::from_ymd(2021, 7, 12))
-        .prompt()
-        .unwrap();
-    println!("{}", date);
+fn date_select_default() {
+    println!("-------> Simple DateSelect");
+    println!();
 
-    let date = DateSelect::new("With week start input")
-        .with_week_start(chrono::Weekday::Mon)
-        .prompt()
-        .unwrap();
-    println!("{}", date);
+    DateSelect::new("Check-in date:").prompt().unwrap();
+    println!("We will be expecting you!");
+    println!();
+}
+
+fn custom_type_parsed_date_prompt() {
+    println!("-------> Date parsed from text input with Custom Type prompt");
+    println!();
+
+    let amount = CustomType::<NaiveDate>::new("When are you going to visit the office?")
+        .with_placeholder("dd/mm/yyyy")
+        .with_parser(&|i| NaiveDate::parse_from_str(i, "%d/%m/%Y").map_err(|_| ()))
+        .with_formatter(DEFAULT_DATE_FORMATTER)
+        .with_error_message("Please type a valid date.")
+        .with_help_message("The necessary arrangements will be made")
+        .prompt();
+
+    match amount {
+        Ok(_) => println!("Thanks! We will be expecting you."),
+        Err(_) => println!("We could not process your reservation"),
+    }
+    println!();
+}
+
+fn date_select_misc_options() {
+    println!("-------> Date select with several possible options");
+    println!();
+
+    let date = DateSelect::new("When do you want to travel?")
+        .with_default(NaiveDate::from_ymd(2021, 8, 1))
+        .with_min_date(NaiveDate::from_ymd(2021, 8, 1))
+        .with_max_date(NaiveDate::from_ymd(2021, 12, 31))
+        .with_week_start(Weekday::Mon)
+        .with_help_message("Possible flights will be displayed according to the selected date")
+        .prompt();
+
+    match date {
+        Ok(_) => println!("No flights available for this date."),
+        Err(_) => println!("There was an error in the system."),
+    }
+    println!();
+}
+
+fn date_select_with_validation() {
+    println!("-------> Date select with date validation");
+    println!();
 
     let date = DateSelect::new("Validated input")
         .with_validator(|d: NaiveDate| {
@@ -28,7 +69,11 @@ fn main() {
                 Ok(Validation::Valid)
             }
         })
-        .prompt()
-        .unwrap();
-    println!("{}", date);
+        .prompt();
+
+    match date {
+        Ok(_) => println!("No flights available for this date."),
+        Err(_) => println!("There was an error in the system."),
+    }
+    println!();
 }
