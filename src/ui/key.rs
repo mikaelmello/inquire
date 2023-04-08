7ed4196f6cb199f193a22a57dfaacbe37eb2a6a6
Mikael Mello <git@mikaelmello.com>
2023-04-07 20:39:42 -0700

    Derive traits for KeyModifiers type

--- src/ui/key.rs
+++ src/ui/key.rs
@@ -3,6 +3,7 @@ use bitflags::bitflags;
 // Using the same struct, but without importing, to cut prompts' direct dependencies to crossterm
 // https://github.com/crossterm-rs/crossterm/blob/e1260446e94e9a8f7809fef61dc1369b6f8d6e12/src/event.rs#L376-L385
 bitflags! {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub struct KeyModifiers: u8 {
         const SHIFT = 0b0000_0001;
         const CONTROL = 0b0000_0010;
