70bf4caa40328bef6aafd5f50985ba6ee7fcf783
Mikael Mello <git@mikaelmello.com>
2023-12-24 15:30:17 -0800

    Fix with_starting_cursor not being respected on selectable prompts (#197)

--- inquire/src/prompts/select/test.rs
+++ inquire/src/prompts/select/test.rs
@@ -93,3 +93,67 @@ fn down_arrow_on_empty_list_does_not_panic() {
 
     assert_eq!(ListOption::new(0, 1), ans);
 }
+
+#[test]
+// Anti-regression test: https://github.com/mikaelmello/inquire/issues/195
+fn starting_cursor_is_respected() {
+    let read: Vec<KeyEvent> = [KeyCode::Enter]
+        .iter()
+        .map(|c| KeyEvent::from(*c))
+        .collect();
+
+    let mut read = read.iter();
+
+    let options = vec![1, 2, 3];
+
+    let mut write: Vec<u8> = Vec::new();
+    let terminal = CrosstermTerminal::new_with_io(&mut write, &mut read);
+    let mut backend = Backend::new(terminal, RenderConfig::default()).unwrap();
+
+    let ans = Select::new("Question", options)
+        .with_starting_cursor(2)
+        .prompt_with_backend(&mut backend)
+        .unwrap();
+
+    assert_eq!(ListOption::new(2, 3), ans);
+}
+
+#[test]
+fn naive_assert_fuzzy_match_as_default_scorer() {
+    let read: Vec<KeyEvent> = [
+        KeyCode::Char('w'),
+        KeyCode::Char('r'),
+        KeyCode::Char('r'),
+        KeyCode::Char('y'),
+        KeyCode::Enter,
+    ]
+    .iter()
+    .map(|c| KeyEvent::from(*c))
+    .collect();
+
+    let mut read = read.iter();
+
+    let options = vec![
+        "Banana",
+        "Apple",
+        "Strawberry",
+        "Grapes",
+        "Lemon",
+        "Tangerine",
+        "Watermelon",
+        "Orange",
+        "Pear",
+        "Avocado",
+        "Pineapple",
+    ];
+
+    let mut write: Vec<u8> = Vec::new();
+    let terminal = CrosstermTerminal::new_with_io(&mut write, &mut read);
+    let mut backend = Backend::new(terminal, RenderConfig::default()).unwrap();
+
+    let ans = Select::new("Question", options)
+        .prompt_with_backend(&mut backend)
+        .unwrap();
+
+    assert_eq!(ListOption::new(2, "Strawberry"), ans);
+}
