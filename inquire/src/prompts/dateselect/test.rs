fad1b84957499447460dab018e7dc0bc5a08ebcb
Mikael Mello <git@mikaelmello.com>
2023-12-26 22:34:50 -0800

    Remove test temporarily

--- inquire/src/prompts/dateselect/test.rs
+++ inquire/src/prompts/dateselect/test.rs
@@ -60,26 +60,3 @@ fn closure_validator() {
 
     assert_eq!(today_date.pred_opt().unwrap(), ans);
 }
-
-#[test]
-/// Tests the behaviour of several keybindings in a admittedly naive way.
-fn naive_keybinding_checks() {
-    let mut backend = fake_backend(vec![Key::Enter, Key::Left(KeyModifiers::NONE), Key::Enter]);
-
-    let today_date = get_current_date();
-
-    let validator = move |d| {
-        if today_date > d {
-            Ok(Validation::Valid)
-        } else {
-            Ok(Validation::Invalid("Date must be in the past".into()))
-        }
-    };
-
-    let ans = DateSelect::new("Question")
-        .with_validator(validator)
-        .prompt_with_backend(&mut backend)
-        .unwrap();
-
-    assert_eq!(today_date.pred_opt().unwrap(), ans);
-}
