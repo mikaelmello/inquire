62dfcb9bf45345a503baf6f862172b484993932c
Mikael Mello <git@mikaelmello.com>
2024-02-23 22:41:17 -0800

    Move API UI files to api dir, keep imports equal

---
+++ inquire/src/ui/api/mod.rs
@@ -0,0 +1,9 @@
+mod color;
+mod key;
+mod render_config;
+mod style;
+
+pub use color::*;
+pub(crate) use key::*;
+pub use render_config::*;
+pub use style::*;
--- inquire/src/ui/mod.rs
+++ inquire/src/ui/mod.rs
@@ -1,18 +1,12 @@
 //! UI-related definitions for rendered content.
 
+mod api;
 mod backend;
-mod color;
 pub(crate) mod dimension;
 mod frame_renderer;
 mod input_reader;
-mod key;
-mod render_config;
-mod style;
 
 pub(crate) use backend::*;
 pub(crate) use input_reader::*;
-pub(crate) use key::*;
 
-pub use color::Color;
-pub use render_config::*;
-pub use style::{Attributes, StyleSheet, Styled};
+pub use api::*;
