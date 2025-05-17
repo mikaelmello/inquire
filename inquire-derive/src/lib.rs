9e7589515fbb714110419dbc6570166cb8ab236e
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-05-16 19:04:23 -0500

    Add `InquireChoose` derive macro

--- inquire-derive/src/lib.rs
+++ inquire-derive/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data};
+
+fn enum_choose_impl(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as syn::DeriveInput);
+    let enum_name = &input.ident;
+
+    let variants = if let Data::Enum(data_enum) = &input.data {
+        data_enum
+            .variants
+            .iter()
+            .map(|v| &v.ident)
+            .collect::<Vec<_>>()
+    } else {
+        panic!("Choose can only be derived for enums");
+    };
+
+    let module_name = format_ident!("__choice_for_{}", enum_name.to_string().to_lowercase());
+
+    let expanded = quote! {
+        mod #module_name {
+            use super::*;
+
+            #[doc(hidden)]
+            pub trait Variants<T: 'static> {
+                const VARIANTS: &'static [T];
+            }
+
+            impl Variants<#enum_name> for #enum_name {
+                const VARIANTS: &'static [#enum_name] = &[#(#variants),*];
+            }
+
+            pub use Variants as VariantsTrait;
+        }
+
+        impl #enum_name {
+            pub fn choose(prompt: &str) -> ::inquire::error::InquireResult<Self>
+            where
+                Self: ::std::fmt::Display + ::std::fmt::Debug + Copy + Clone + 'static,
+            {
+                let answer: Self = ::inquire::Select::new(msg, <Self as #module_name::VariantsTrait<Self>>::VARIANTS.to_vec())
+                    .prompt()?;
+
+                Ok(answer)
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_derive(InquireChoose)]
+pub fn derive_choose(input: TokenStream) -> TokenStream {
+    enum_choose_impl(input)
 }
